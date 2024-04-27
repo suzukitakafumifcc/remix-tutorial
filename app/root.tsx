f8c474d359dbe1da82039c4815b141c97fddfa83
Suzuki Takafumi <suzukitakafumifcc@gmail.com>
2024-04-27 13:47:03 +0900

    feat: delete contact

--- app/root.tsx
+++ app/root.tsx
@@ -1,13 +1,14 @@
-import { json } from "@remix-run/node";
+import { json, redirect } from "@remix-run/node";
 import {
   Form,
-  Link,
   Links,
   Meta,
+  NavLink,
   Outlet,
   Scripts,
   ScrollRestoration,
   useLoaderData,
+  useNavigation,
 } from "@remix-run/react";
 import type { LinksFunction } from "@remix-run/node";
 import appStylesHref from './app.css?url';
@@ -15,7 +16,7 @@ import { createEmptyContact, getContacts } from "./data";
 
 export const action = async () => {
   const contact = await createEmptyContact();
-  return json({ contact })
+  return redirect(`contacts/${contact.id}/edit`);
 };
 
 export const links: LinksFunction = () => [
@@ -29,6 +30,7 @@ export const loader = async () => {
 
 export default function App() {
   const { contacts } = useLoaderData<typeof loader>();
+  const navigation = useNavigation();
 
   return (
     <html lang="en">
@@ -62,13 +64,21 @@ export default function App() {
                 {
                   contacts.map((contact) => (
                     <li key={contact.id}>
-                      <Link to={`/contacts/${contact.id}`}>
+                      <NavLink
+                        className={({ isActive, isPending }) =>
+                          isActive
+                            ? "active"
+                            : isPending
+                              ? "pending"
+                              : ""
+                        }
+                        to={`/contacts/${contact.id}`}>
                         {contact.first || contact.last ? (
                           <>
                             {contact.first} {contact.last}
                           </>
                         ) : null}
-                      </Link>
+                      </NavLink>
                     </li>
                   ))
                 }
@@ -77,7 +87,10 @@ export default function App() {
             )} {" "}
           </nav>
         </div>
-        <div id="detail">
+        <div
+          className={navigation.state === "loading" ? "loading" : ""}
+          id="detail"
+        >
           <Outlet />
         </div>
         <ScrollRestoration />
