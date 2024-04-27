baf3cb7839f06a9abc08099bdf9578fa603907d1
Suzuki Takafumi <suzukitakafumifcc@gmail.com>
2024-04-27 13:50:42 +0900

    feat: search contact

--- app/root.tsx
+++ app/root.tsx
@@ -10,7 +10,7 @@ import {
   useLoaderData,
   useNavigation,
 } from "@remix-run/react";
-import type { LinksFunction } from "@remix-run/node";
+import type { LinksFunction, LoaderFunctionArgs, } from "@remix-run/node";
 import appStylesHref from './app.css?url';
 import { createEmptyContact, getContacts } from "./data";
 
@@ -23,8 +23,10 @@ export const links: LinksFunction = () => [
   { rel: 'stylesheet', href: appStylesHref }
 ];
 
-export const loader = async () => {
-  const contacts = await getContacts();
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  const url = new URL(request.url);
+  const q = url.searchParams.get("q");
+  const contacts = await getContacts(q);
   return json({ contacts });
 };
 
